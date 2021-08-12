79408c09aaee3564e5b78db29d2c684587aa0757
Nobu <nowobuwu@gmail.com>
2021-08-11 23:43:02 -0500

    add discord.js version & node version verifier

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-const version = "1.0.1"
-import Client from './Client'
-import ClusterUtil from './cluster/ClusterUtil' 
-
 try {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const djs: typeof import('discord.js') = require('discord.js')
@@ -12,6 +8,16 @@ try {
 } catch (error) {
   console.log("Discord.js is required to use this library!\n", error)
 }
+const nodev = process.version.replace("v", "").split(".")
+if (parseInt(nodev[0]) < 16) {
+  throw Error("Node >=16.6.0 is required to use this package!")
+} else if (parseInt(nodev[1]) < 6) {
+  throw Error("Node >=16.6.0 is required to use this package!")
+}
+
+const version = process.env.npm_package_version
+import Client from './Client'
+import ClusterUtil from './cluster/ClusterUtil' 
 
 export = {
   version,
